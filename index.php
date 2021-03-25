5f1c67e636370a791ec1ea9d765381a6b7718a24
ratchabala <ratchabala@gmail.com>
2021-03-25 23:31:24 +0530

    sree

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Connection | Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel = "stylesheet" href = "styleindex.css">
+</head>
+<body class = "bg-dark">
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-end">
+<a class="navbar-brand" href="#">
+    <img src="images/logo.jpeg" alt="logo" style="width:100px;height:50px">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+  
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">About</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Find a mentor</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Be a mentor</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Contact</a>
+    </li>
+  </ul>
+  </div>  
+</nav>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-end">
+<a class="navbar-brand" href="#">
+    <img src="images/logo.jpeg" alt="logo" style="width:100px;height:50px">
+  </a>
+ 
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 2</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 3</a>
+    </li>
+  </ul>
+
+</nav>
+</body>
+</html>
+
+
