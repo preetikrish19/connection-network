b1ef6e0aa08ebd9f40ee7cfbeb578636d3fb1c01
hemagunnala <gunnalahema14134@gmail.com>
2021-03-26 09:58:32 +0530

    hemaa

--- domain.php
+++ domain.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="sytle.css">
 <style>
 body {
   background-image: url(background.jpeg);
@@ -10,5 +11,27 @@ body {
 </style>
 </head>
 <body>
+<div class="card">
+  <img class="card-img-top"style="width: 18rem;height: 18rem" src="images/c ++.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card">
+  <img class="card-img-top"style="width: 18rem;height: 18rem" src="images/dbms.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card">
+  <img class="card-img-top"style="width: 18rem;height: 18rem ;top:20px;left:400px;" src="images/dsa.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
 </body>
 </html>
